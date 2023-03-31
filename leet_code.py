d110ec94880f13632f6186a8a06c20fd4dcb9438
karanlongani <karanlongani8@gmail.com>
2023-03-31 19:30:54 +0530

    functional call

--- leet_code.py
+++ leet_code.py
@@ -13,111 +13,132 @@ options.add_argument("--start-maximized")
 options.binary_location = r'C:\Program Files\Google\Chrome Beta\Application\chrome.exe'
 browser = webdriver.Chrome(options = options, service = Service("chromedriver.exe"))
 
-
 browser.get("https://leetcode.com/problemset/all/")
 time.sleep(5)
 
-dif=browser.find_element(By.XPATH , "//button[contains(text(), 'Difficulty')]")
-time.sleep(5)
 
-browser.execute_script("arguments[0].scrollIntoView();",dif)
-time.sleep(5)
+def choice_(difficulty_ , status_):
 
-dif.click()
-time.sleep(5)
+    diff = browser.find_element(By.XPATH , "//button[contains(text(), 'Difficulty')]")
 
-med = browser.find_element(By.XPATH , "//span[contains(text(), 'Medium')]")
-med.click()
-time.sleep(5)
-
-status=browser.find_element(By.XPATH , "//button[contains(text(), 'Status')]")
-status.click()
-time.sleep(5)
-
-# # # todo=browser.find_element(By.XPATH , "//span[contains(text(), 'Todo')]")
-# # # print("------------")
-# # # print(todo)
-# # # print("------------")
-
-todo=browser.find_element(By.XPATH , "//span[text()='Todo']")
-# # #print("------------")
-# # print(todo)
-# # #print("------------")
-
-todo.click()
-time.sleep(5)
-
-tags=browser.find_element(By.XPATH , "//span[text()='Tags']")
-tags.click()
-time.sleep(5)
-
-kano=['Greedy','Array']
-
-for x in kano:
-    
-    arr_gre=browser.find_element(By.XPATH , f"//span[@data-name='{x}']")
-    arr_gre.click()
+    browser.execute_script("arguments[0].scrollIntoView();",diff)
+    diff.click()
     time.sleep(5)
 
-tags=browser.find_element(By.XPATH , "//span[text()='Tags']")
-tags.click()
-time.sleep(10)
-# browser.get("https://leetcode.com/problemset/all/?difficulty=MEDIUM&page=1&status=NOT_STARTED&topicSlugs=greedy%2Carray")
-# time.sleep(10)
+    type_of_diff = browser.find_element(By.XPATH , f"//span[contains(text(), '{difficulty_}')]")
+    type_of_diff.click()
+    time.sleep(5)
 
-#all_titles=browser.find_elements(By.XPATH,"//div[@class='flex items-center']")
+    # difficulty_ = "Medium" , "Easy" , "Hard" .
 
-#for kk in all_titles:
- #   print(kk.text)
-    
+    status = browser.find_element(By.XPATH , "//button[contains(text(), 'Status')]")
+    status.click()
+    time.sleep(5)
 
-#accepts=browser.find_elements(By.XPATH,"//div[@style='box-sizing:border-box;flex:100 0 auto;min-width:0px;width:100px']")
-#for kk in accepts:
-#    print(kk.text)
+    type_of_status_ = browser.find_element(By.XPATH , f"//span[text()='{status_}']")
+    type_of_status_.click()
+    time.sleep(5)
 
+    # status_ = "Todo" , "Solved" , "Attempted" .
 
-main_data=[]
+    tags=browser.find_element(By.XPATH , "//span[text()='Tags']")
+    tags.click()
+    time.sleep(5)
 
+    kano=['Greedy','Array']
 
-while(True):
-
-    all_items=browser.find_elements(By.XPATH,"//div[@role='row']")
-    
-    for x in all_items:
+    for x in kano:
         
-        try:
-            dic={}
-            all_titles=x.find_element(By.CLASS_NAME,'truncate')
-            all_acceptance=x.find_element(By.TAG_NAME,'span')
-
-            print(all_titles.text)
-            print(all_acceptance.text)
+        arr_gre=browser.find_element(By.XPATH , f"//span[@data-name='{x}']")
+        arr_gre.click()
+        time.sleep(5)
 
 
-            dic["title"]=all_titles.text
-            dic["accept"]=all_acceptance.text
-            main_data.append(dic)
-        except Exception as e:
-            print(e)
+    tags=browser.find_element(By.XPATH , "//span[text()='Tags']")
+    tags.click()
+    time.sleep(5)
 
-    next=browser.find_element(By.XPATH,"//button[@aria-label='next']")
-    last_page = next.get_attribute("disabled")
-    #print(last_page)
 
-    if last_page=="true":
-        break
+
+def data_(file_name):
+    main_data=[]
+
+
+    while(True):
+
+        all_items=browser.find_elements(By.XPATH,"//div[@role='row']")
+    
+        for x in all_items:
+        
+            try:
+                dic={}
+                all_titles=x.find_element(By.CLASS_NAME,'truncate')
+                all_acceptance=x.find_element(By.TAG_NAME,'span')
+
+                print(all_titles.text)
+                print(all_acceptance.text)
+
+
+                dic["title"]=all_titles.text
+                dic["accept"]=all_acceptance.text
+                main_data.append(dic)
+
+            except Exception as e:
+                print(e)
+
+        next = browser.find_element(By.XPATH,"//button[@aria-label='next']")
+        last_page = next.get_attribute("disabled")
+        #print(last_page)
+
+        if last_page=="true":
+            break
     
 
-    browser.execute_script("arguments[0].scrollIntoView();",next)
-    next.click()
+        browser.execute_script("arguments[0].scrollIntoView();",next)
+        next.click()
 
-    time.sleep(10)
+        time.sleep(10)
 
-pd.DataFrame(main_data).to_excel("leet_code.xlsx",index=False)
-
-
-
-
-browser.quit()
-
+    # file_name = give a name of your file name
+    pd.DataFrame(main_data).to_excel(f"{file_name}.xlsx",index=False)
     
+
+algorithms_ = browser.find_element(By.XPATH , "//div[contains(text(), 'Algorithms')]")
+algorithms_.click()
+time.sleep(5)
+
+choice_('Easy' , 'Todo')
+data_('algorithms_')
+
+
+database_ = browser.find_element(By.XPATH , "//div[contains(text(), 'Database')]")
+database_.click()
+time.sleep(5)
+
+choice_('Easy' , 'Todo')
+data_('database_')
+
+
+shell_ = browser.find_element(By.XPATH , "//div[contains(text(), 'Shell')]")
+shell_.click()
+time.sleep(5)
+
+choice_('Easy' , 'Todo')
+data_('shell_')
+
+Concurrency_ = browser.find_element(By.XPATH , "//div[contains(text(), 'Concurrency')]")
+Concurrency_.click()
+time.sleep(5)
+
+choice_('Easy' , 'Todo')
+data_('Concurrency')
+
+
+
+
+
+
+
+
+
+
